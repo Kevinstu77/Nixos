7374e3857ba0f03269a09313a3b3044f7926347c
colby <127161373+Kevinstu77@users.noreply.github.com>
2026-08-08 21:01:39 -0400

    new lap

---
+++ Hosts/Hp-Omen-16-Max/configuration.nix
@@ -0,0 +1,77 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      ../../Modules/Porter.nix
+    ];
+
+  #activate toggles
+  #pkgs
+  myConfig.gaming.enable = true;
+  myConfig.Record.enable = true;
+
+  #services
+  myConfig.local.enable = true;
+  myConfig.audio.enable = true;
+  myConfig.KDE.enable = true;
+  myConfig.Kernel.enable = false;
+  myConfig.GpuDrivers = {
+    enable = false;
+    optimus = {
+      enable = false;
+      nvidiaBusId = "PCI:1@0:0:0";
+      amdgpuBusId = "PCI:115@0:0:0";
+    };
+  };
+
+  nixpkgs.config.allowUnfree = true;
+  #features
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "Hp-Omen-16-Max"; # Define your hostname.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users."colbys" = {
+    isNormalUser = true;
+    description = "Colby Stults";
+    initialPassword = "4850";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    ];
+  };
+
+
+  #Enable Services
+  services.flatpak.enable = true;
+  services.openssh.enable = true;
+
+  #Enable programs
+  programs.firefox.enable = true;
+
+  #Install system packages
+  environment.systemPackages = with pkgs; [
+    kdePackages.kate
+    git
+    pkgs.mission-center
+    s-tui
+    gh
+  ];
+
+  #secret
+  #age.secrets.colbys-password.file = ../../secrets/colbys-password.age;
+
+  system.stateVersion = "26.05";
+
+}
