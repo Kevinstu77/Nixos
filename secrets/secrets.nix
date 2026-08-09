6a01db5ebca0e30ae84a5e96bc70f0b721582996
colby <127161373+Kevinstu77@users.noreply.github.com>
2026-08-08 21:22:55 -0400

    secret

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,8 @@
 let
   Lenovo-Slim-Pro-7 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKHZLWSFB3lHTzJ96T12czsPv5DVMU2MFDEABavhdHXb root@Lenovo-Slim-Pro-7";
+  Hp-Omen-16-Max = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHYJZrO0ObtYp95qf5jbTzgTt7m5lYk/s78Y+u5YnMTz Hp-Omen-16-Max";
+
 in
 {
-  "colbys-password.age".publicKeys = [ Lenovo-Slim-Pro-7 ];
+  "colbys-password.age".publicKeys = [ Lenovo-Slim-Pro-7 Hp-Omen-16-Max ];
 }
