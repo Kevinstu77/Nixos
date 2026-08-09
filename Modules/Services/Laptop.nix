d44e97a7d30cbd8373eb178476216d3b3f984400
colby <127161373+Kevinstu77@users.noreply.github.com>
2026-08-09 17:39:41 -0400

    power

--- Modules/Services/Laptop.nix
+++ Modules/Services/Laptop.nix
@@ -11,18 +11,18 @@ in
     enable = mkEnableOption "Enable laptop max performance";
   };
 
-  config = mkIf cfg.enable {
-
-    programs.auto-cpufreq.enable = true;
-    programs.auto-cpufreq.settings = {
-    charger = {
-      governor = "performance";
-      turbo = "auto";
-    };
-
+config = mkIf cfg.enable {
+  services.power-profiles-daemon.enable = false;
+  services.auto-cpufreq.enable = true;
+  services.auto-cpufreq.settings = {
     battery = {
       governor = "performance";
-      turbo = "auto";
+      turbo = "never";
     };
+  charger = {
+    governor = "performance";
+    turbo = "auto";
+   };
   };
+ };
 }
